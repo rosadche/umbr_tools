6f7f9e224b99d2ad4fe18e1810177195f07d43d8
Reilly Osadchey Brown <rosadche@bu.edu>
2023-06-08 10:17:18 -0400

    updated umbr_1d.py

--- umbr_1d.py
+++ umbr_1d.py
@@ -55,11 +55,37 @@ class umbrella_set(object):
         else:
             self.temps = np.full(self.num_im, temp)
 
-    def cv_file(self, file_base):
-        self.cv_files = [file_base.format(i=x) for x in range(self.num_im)]
+    def cv_file(self, files, file_type):
+        """
+        files can be:
+            - a string with {i} for format (range up to num_im),
+            - a list/array of lenght num_images
+        file_type can be:
+            "colvar": a plumed COLVAR file
+            "df": a pandas dataframe directly
+            "df_pkl": a pickle of a pandas dataframe
+        """
+
+        if istype(files, str):
+            self.cv_files = [file_base.format(i=x) for x in range(self.num_im)]
+        elif istype(files, list) or istype(files, np.ndarray):
+            if len(files) == self.num_im:
+                self.cv_files = files
+            else:
+                raise Exception("List/Array is the wrong length!")
+        else:
+            raise Exception("Files not provided as correct type!")
+
+        filetype_options = ["colvar", "df", "df_pkl"]
+        if file_type.lower() not in filetype_options:
+            raise Exception(
+                "file_type muse be  not one of: {}!".format(", ".join(filetype_options))
+            )
+        else:
+            self.file_type = file_type
 
 
-class adaptive_umbrella_collection(object):
+class umbrella_collection(object):
 
     """
     An Umbrella Collection is defined as any number of umbrella sets.
@@ -67,7 +93,7 @@ class adaptive_umbrella_collection(object):
     An umbrella collection therefore is used for when you have different static biases
     """
 
-    def __init__(self, dict_umbrella_sets):
+    def __init__(self, dict_umbrella_sets, kB, units):
         # takes a dictionary of umbrella sets
         if not isinstance(dict_umbrella_sets, dict):
             raise Exception(
@@ -77,6 +103,18 @@ class adaptive_umbrella_collection(object):
             # ----- Parse Key Data from the dictionary data of Umbrella Sets ---------
             self.dict_umbr_sets = dict_umbrella_sets
             self.umbr_sets_keys = list(self.dict_umbr_sets.keys())
+
+            if istype(kB, float):
+                self.kB = kB
+            else:
+                raise Exception("kB must be a float value!")
+
+            if istype(units, str):
+                self.units = units
+            else:
+                raise Exception(
+                    "units must be a string (just for clarifying in output files)!"
+                )
 
             self.K_per_set = np.asarray(
                 [self.dict_umbr_sets[x].num_im for x in self.umbr_sets_keys]
@@ -138,10 +176,10 @@ class adaptive_umbrella_collection(object):
         # run the plumed command to do calculation
         print("\nRunning Plumed Driver To Get External Biases")
         message = os.popen(f"plumed driver --noatoms --plumed plumed.dat").readlines()
-        # print( "".join(message) )
+        if self.verbose:
+            print("".join(message))
         # load the data back in
         bias_df = read_colvar("colvar_all_bias", keep_zero=True)
-        print(bias_df)
         bias_df.drop(labels=["time"], axis="columns", inplace=True)
         self.bias_df = bias_df
 
@@ -152,6 +190,9 @@ class adaptive_umbrella_collection(object):
         shutil.rmtree("./reweight_scratch")
 
     def do_mbar(self, umbr_options=None, mbar_options=None):
+        """ """
+
+        # start with options
         needed_umbr_options = (
             "cv_col",
             "N_max",
@@ -161,8 +202,6 @@ class adaptive_umbrella_collection(object):
             "g",
             "verbose",
             "outfile",
-            "units",
-            "kB",
         )
 
         if umbr_options == None:
@@ -171,10 +210,37 @@ class adaptive_umbrella_collection(object):
             if option not in needed_umbr_options:
                 umbr_options[option] = None
 
+        # Required Arguements (No Default Value)
         if umbr_options["cv_col"] == None:
             raise Exception("Must provide cv_col!")
         else:
-            self.cv_col = cv_col
+            self.cv_col = umbr_options["cv_col"]
+
+        if umbr_options["N_max"] == None:
+            raise Exception("Must provide N_max!")
+        else:
+            self.N_max = umbr_options["N_max"]
+
+        if umbr_options["outtemp"] == None:
+            raise Exception("Must provide outtemp (in Kelvin)!")
+        else:
+            self.N_max = umbr_options["outtemp"]
+
+        # Optional Arguements (Default Values)
+        if umbr_options["nbins"] == None:
+            self.nbins = 100
+        else:
+            self.nbins = umbr_options["nbins"]
+
+        if umbr_options["pot_ener_col"] == None:
+            self.pot_ener_col = None
+            # if all temperatures are not all the same, must provide a potential energy column in COLVAR fikle
+            if not (np.all(self.T_k == self.T_k[0])):
+                raise Exception(
+                    "A column containing potential energy must be given when reweighting with simulations at different temperatures!"
+                )
+        else:
+            self.pot_ener_col = umbr_options["pot_ener_col"]
 
         if umbr_options["verbose"] == None:
             self.verbose = True
@@ -186,31 +252,26 @@ class adaptive_umbrella_collection(object):
         else:
             self.outfile = umbr_options["outfile"]
 
-        # if all temperatures are not all the same, must provide a potential energy column in COLVAR fikle
-        if (self.pot_ener_col == None) and not (np.all(self.T_k == self.T_k[0])):
-            raise Exception(
-                "A column containing potential energy must be given when reweighting with simulations at different temperatures!"
-            )
-
         # if g is a constant, lets fill the g_k array with it
         # if g is a list or array, make sure it is correct length and if so make that the g_k array
-        # if g=None, make sure to initialize the array anyway
-        self.g_k = np.zeros([self.K])  # statistical inefficiency of simualtion k
-        if g != None:
-            if isinstance(g, list) or isinstance(g, np.ndarray):
-                if len(g) != self.K:
+        # if g is None, then later we will use pymbar's timeseries module to calculate it
+        self.g = umbr_options["g"]
+        self.g_k = np.zeros([self.K])  # statistical inefficiency of simulation k
+        if self.g != None:
+            if isinstance(self.g, list) or isinstance(self.g, np.ndarray):
+                if len(self.g) != self.K:
                     raise Exception(
-                        "g as an array must be list/array of integers with len of total windows!"
+                        "g as an array/list must be of same length as number of total windows!"
                     )
                 else:
-                    self.g_k[:] = g
+                    self.g_k[:] = self.g
             else:
-                if not isinstance(g, int):
+                if not isinstance(self.g, int):
                     raise Exception(
                         "g must bean integer, an list/array of integers, or None"
                     )
                 else:
-                    self.g_k[:] = g
+                    self.g_k[:] = self.g
 
         # ------ Allocate Storage --------------------------------------------------------------
         self.N_k = np.zeros(
@@ -222,18 +283,18 @@ class adaptive_umbrella_collection(object):
         )  # Retraint_k[k] is the Umbrella spring constant and center vals for simualtion k: r1, k1
 
         self.cv_mat_kn = np.zeros(
-            [self.K, N_max]
+            [self.K, self.N_max]
         )  # cv_mat_kn[k,n] is the CV value for snapshot n from umbrella simulation k
         self.cv_mat_kn[:] = np.nan
 
         self.u_kn = np.zeros(
-            [self.K, N_max]
+            [self.K, self.N_max]
         )  # u_kn[k,n] is the reduced potential energy without umbrella restraints of snapshot n of umbrella simulation k (only needed if T is not constant)
         self.u_kln = np.zeros(
-            [self.K, self.K, N_max]
+            [self.K, self.K, self.N_max]
         )  # u_kln[k,l,n] is the reduced potential energy of snapshot n from umbrella simulation k evaluated at umbrella l
         self.b_kln = np.zeros(
-            [self.K, self.K, N_max]
+            [self.K, self.K, self.N_max]
         )  # b_kln[k,l,n] is the static bias of snapshot n (from umbrella simulation k) evaluated at umbrella l
 
         # ------ Load Data --------------------------------------------------------------
@@ -257,10 +318,21 @@ class adaptive_umbrella_collection(object):
             )
             self.restraint_k[k, :] = cv_details
 
-            file = self.dict_umbr_sets[which_set].cv_files[image_in_set]
-            df = pd.read_pickle(file)
+            if self.dict_umbr_sets[which_set].file_type == "colvar":
+                df = read_colvar(
+                    self.dict_umbr_sets[which_set].cv_files[image_in_set],
+                    keep_zero=False,
+                )
+            if self.dict_umbr_sets[which_set].file_type == "df":
+                df = self.dict_umbr_sets[which_set].cv_files[image_in_set]
+            if self.dict_umbr_sets[which_set].file_type == "df_pkl":
+                df = pd.read_pickle(
+                    self.dict_umbr_sets[which_set].cv_files[image_in_set]
+                )
+            else:
+                raise Exception("This CV filetype has not been implemented!")
 
-            cv_vals = df[cv_col].to_numpy()
+            cv_vals = df[self.cv_col].to_numpy()
             self.cv_mat_kn[k, 0 : len(cv_vals)] = cv_vals
 
             if pot_ener_col:
@@ -315,7 +387,10 @@ class adaptive_umbrella_collection(object):
                     counter += self.N_k[k]
 
         hist, bins = np.histogram(
-            self.cv_mat_kn, bins=nbins, range=(self.cv_min, self.cv_max), density=False
+            self.cv_mat_kn,
+            bins=self.nbins,
+            range=(self.cv_min, self.cv_max),
+            density=False,
         )
         cv_min, cv_max = check_histogram(hist, bins)
 
@@ -336,9 +411,9 @@ class adaptive_umbrella_collection(object):
         )
 
         # compute bin centers
-        bin_center_i = np.zeros([nbins])
-        bin_edges = np.linspace(cv_min, cv_max, nbins + 1)
-        for i in range(nbins):
+        bin_center_i = np.zeros([self.nbins])
+        bin_edges = np.linspace(cv_min, cv_max, self.nbins + 1)
+        for i in range(self.nbins):
             bin_center_i[i] = 0.5 * (bin_edges[i] + bin_edges[i + 1])
 
         N = np.sum(self.N_k)
@@ -372,7 +447,7 @@ class adaptive_umbrella_collection(object):
         text = f"# free energy profile from histogramming" + "\n"
         text += f"# provided units: {self.units}" + "\n"
         text += f"# provided value for kB: {self.kB}"
-        text += f"# provided T={self.outtemp} K, resulitng in KT={self.kT} {self.units}"
+        text += f"# provided T={self.outtemp} K, resulitng in kT={self.kT} {self.units}"
         text += f"{'bin':>8s} {'f':>8s} {'df':>8s}" + "\n"
         for i in range(nbins):
             text += (
